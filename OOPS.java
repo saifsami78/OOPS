11731f1a5d2bc3016006095dadd9b4cd6c70132e
Saif Sami <saif.sarker@northsouth.edu>
2026-03-03 00:00:23 +0600

    checking

--- OOPS.java
+++ OOPS.java
@@ -12,7 +12,16 @@ class LibraryBook{
     LibraryBook(String title, String Author){
         this.title = title;
         this.Author = Author;
+        this.isAvailable = true;
+    }
+
+    void SetTitle(Sting NewTitle){
+        this.title = NewTitle;
+    }
+
+    void SetAuthor(String NewAuthor){
         
     }
+
 
 }
