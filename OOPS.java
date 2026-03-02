5eb8fea5abd5aff6682074f211587640aa41c413
Saif Sami <saif.sarker@northsouth.edu>
2026-03-03 01:15:15 +0600

    solved the problem

--- OOPS.java
+++ OOPS.java
@@ -1,5 +1,15 @@
 public class OOPS{
-       
+
+    public static void main(String[] args) {
+        LibraryBook AkashVoraTara = new LibraryBook("Akash Vora Tara" ,"Humayun Ahmed");
+        LibraryBook DBMS = new LibraryBook("Easy Mysql", "Abdul Bari");
+
+        AkashVoraTara.borrowBook();
+        AkashVoraTara.borrowBook();
+        AkashVoraTara.returnBook();
+        AkashVoraTara.getInfo();    
+    }
+    
 }
 
 
@@ -15,13 +25,56 @@ class LibraryBook{
         this.isAvailable = true;
     }
 
-    void SetTitle(Sting NewTitle){
+    void setTitle(String NewTitle){
         this.title = NewTitle;
     }
 
-    void SetAuthor(String NewAuthor){
-        
+    void setAuthor(String NewAuthor){
+        this.Author = NewAuthor;
     }
 
+    String getTitle(LibraryBook Book1){
+        return this.title;
+    }
+
+    String getAuthor(){
+        return this.Author;
+    }
+
+    void getInfo(){
+        System.out.println("Title : " + this.title);
+        System.out.println("Author : " + this.Author);
+        System.out.print("Status : " );
+        if(isAvailable == true){
+            System.out.println("Available");
+        }
+        else{
+            System.out.println("Not Available");
+
+        }
+
+    }
+
+    void borrowBook(){
+        if(isAvailable == true){
+            isAvailable = false;
+            System.out.println("Congrats! You just borrowed the Books");
+        }
+        else{
+            System.out.println("You have already Borrowed that books");
+
+        }
+    }
+
+    void returnBook(){
+        if(isAvailable == false){
+            isAvailable = true;
+            System.out.println("Book Title : "+ this.title +" has been  successfully returned.");
+        }
+        else{
+            System.out.println("You have not borrowed it yet");
+
+        }
+    }
 
 }
